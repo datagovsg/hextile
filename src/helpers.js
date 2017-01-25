d68476a81ef7bc3b871119bc1e05568b966c96f0
Yong Jun <yjthong.2009@smu.edu.sg>
2017-01-26 00:37:43 +0800

    Rewrite with new logic

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,34 @@
-export function getAngularDistance (radius = 6371000) {
-  return function (center, width) {
-    const rad2deg = 180 / Math.PI
-    const ty = width / radius * rad2deg
-    const tx = width / (Math.cos(center[1] / rad2deg) * radius) * rad2deg
-    return [tx, ty]
+export function equirectangular (center, width, radius = 6371000) {
+  const rad2deg = 180 / Math.PI
+  const dy = width / radius * rad2deg
+  const dx = width / (Math.cos(center[1] / rad2deg) * radius) * rad2deg
+  return {
+    forward ([lng, lat]) {
+      return [lng / dx, lat / dy]
+    },
+    back ([x, y]) {
+      return [x * dx, y * dy]
+    }
+  }
+}
+
+export function polar2cartesian (theta) {
+  return [
+    Math.sin(theta / 180 * Math.PI),
+    -Math.cos(theta / 180 * Math.PI)
+  ]
+}
+
+export function dotProduct (v1, v2) {
+  return v1.reduce((sum, e, i) => sum + e * v2[i], 0)
+}
+
+export function linearSolver ([alpha1, beta1], [alpha2, beta2]) {
+  const DET = alpha1 * beta2 - alpha2 * beta1
+  return function (k1, k2) {
+    return [
+      (beta2 * k1 - beta1 * k2) / DET,
+      (-alpha2 * k1 + alpha1 * k2) / DET
+    ]
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import min from 'lodash/min'
 import max from 'lodash/max'
 
-import {getAngularDistance} from './helpers'
+import {equirectangular, polar2cartesian, dotProduct, linearSolver} from './helpers'
 
 /**
  * @param {(Object|Object[])} geojson - https://tools.ietf.org/html/rfc7946
- * @param {('square'|'hexagon|hexagonA|hexagonB')} options.shape - default 'square'
+ * @param {('square'|'hexagon')} options.shape - default 'square'
+ * @param {number} options.tilt - default 0
  * @param {number} options.width - default 1000, min 500, max 50000
- * @param {[number, number]} options.center - [lng, lat] of grid origin
- * @param {Function} options.getAngularDistance - optional, maps width to angular distance
+ * @param {[number, number]} options.center - [lon, lat] of grid origin
+ * @param {Object} options.projection - optional, overwrites center and width
+ * @param {Function} options.projection.forward - map lonlat to grid coordinates
+ * @param {Function} options.projection.inverse - map grid XY to lonlat
  */
 module.exports = function (geojson, options) {
   // normalize input
-  const input = []
+  let input = []
   function extractPolygons (node) {
     if (typeof node !== 'object') return
     if (node instanceof Array) {
@@ -31,124 +34,182 @@ module.exports = function (geojson, options) {
   }
   extractPolygons(geojson)
 
+  input = input.map(coordinates => ({
+    coordinates,
+    bbox: [
+      min(coordinates[0], point => point[0]),
+      min(coordinates[0], point => point[1]),
+      max(coordinates[0], point => point[0]),
+      max(coordinates[0], point => point[1])
+    ]
+  }))
+
   const bbox = [
-    min(input.map(polygon => min(polygon[0], point => point[0]))),
-    min(input.map(polygon => min(polygon[0], point => point[1]))),
-    max(input.map(polygon => max(polygon[0], point => point[0]))),
-    max(input.map(polygon => max(polygon[0], point => point[1])))
+    min(input.map(polygon => polygon.bbox[0])),
+    min(input.map(polygon => polygon.bbox[1])),
+    max(input.map(polygon => polygon.bbox[2])),
+    max(input.map(polygon => polygon.bbox[3]))
   ]
 
   options.shape = options.shape || 'square'
+  options.tilt = options.tilt || 0
   options.width = options.width || 1000
   options.width = Math.max(options.width, 500)
   options.width = Math.min(options.width, 20000)
   options.center = options.center || [(bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2]
-  options.getAngularDistance = options.getAngularDistance || getAngularDistance()
+  options.projection = options.projection || equirectangular(options.center, options.width)
 
-  // populate grid
-  let [dx, dy] = options.getAngularDistance(options.center, options.width)
-  if (options.shape === 'hexagon' || options.shape === 'hexagonA') {
-    dy = dy * Math.sqrt(3) / 2
-  } else if (options.shape === 'hexagonB') {
-    dx = dx * Math.sqrt(3) / 2
-  }
+  const forward = options.projection.forward
+  const inverse = options.projection.inverse
 
-  const output = []
+  let output = {}
+
+  const beta0 = polar2cartesian(options.tilt)
 
   if (options.shape === 'square') {
-    const minX = Math.floor((bbox[0] - options.center[0]) / dx)
-    const maxX = Math.ceil((bbox[2] - options.center[0]) / dx)
-    const minY = Math.floor((bbox[1] - options.center[1]) / dy)
-    const maxY = Math.ceil((bbox[3] - options.center[1]) / dy)
-    for (let nx = minX; nx <= maxX; nx++) {
-      for (let ny = minY; ny <= maxY; ny++) {
-        const x = options.center[0] + nx * dx
-        const y = options.center[1] + ny * dy
-        output.push({
-          type: 'Polygon',
-          id: [nx, ny].join('.').replace(/-/g, 'M'),
-          properties: {
-            address: [x, y]
-          },
-          coordinates: [[
-            [x + 0.5 * dx, y + 0.5 * dy],
-            [x - 0.5 * dx, y + 0.5 * dy],
-            [x - 0.5 * dx, y - 0.5 * dy],
-            [x + 0.5 * dx, y - 0.5 * dy]
-          ]]
-        })
-      }
-    }
-  } else if (options.shape === 'hexagon' || options.shape === 'hexagonA') {
-    const minX = Math.floor((bbox[0] - options.center[0]) / (dx * 0.75))
-    const maxX = Math.ceil((bbox[2] - options.center[0]) / (dx * 0.75))
-    for (let nx = minX; nx <= maxX; nx++) {
-      const cy = options.center[1] + nx * dy * 0.5
-      const minY = Math.floor((bbox[1] - cy) / dy)
-      const maxY = Math.ceil((bbox[3] - cy) / dy)
-      for (let ny = minY; ny <= maxY; ny++) {
-        const x = options.center[0] + nx * dx * 0.75
-        const y = cy + ny * dy
-        output.push({
-          type: 'Polygon',
-          id: [nx, ny].join('.').replace(/-/g, 'M'),
-          properties: {
-            address: [x, y]
-          },
-          coordinates: [[
-            [x + 0.5 * dx, y],
-            [x + 0.25 * dx, y + 0.5 * dy],
-            [x - 0.25 * dx, y + 0.5 * dy],
-            [x - 0.5 * dx, y],
-            [x - 0.25 * dx, y - 0.5 * dy],
-            [x + 0.25 * dx, y - 0.5 * dy]
-          ]]
-        })
-      }
-    }
-  } else if (options.shape === 'hexagonB') {
-    const minY = Math.floor((bbox[1] - options.center[1]) / (dy * 0.75))
-    const maxY = Math.ceil((bbox[3] - options.center[1]) / (dy * 0.75))
-    for (let ny = minY; ny <= maxY; ny++) {
-      const cx = options.center[0] + ny * dx * 0.5
-      const minX = Math.floor((bbox[0] - cx) / dx)
-      const maxX = Math.ceil((bbox[2] - cx) / dx)
-      for (let nx = minX; nx <= maxX; nx++) {
-        const x = cx + nx * dx
-        const y = options.center[1] + ny * dy * 0.75
-        output.push({
-          type: 'Polygon',
-          id: [nx, ny].join('.').replace(/-/g, 'M'),
-          properties: {
-            address: [x, y]
-          },
-          coordinates: [[
-            [x, y + 0.5 * dy],
-            [x - 0.5 * dx, y + 0.25 * dy],
-            [x - 0.5 * dx, y - 0.25 * dy],
-            [x, y - 0.5 * dy],
-            [x + 0.5 * dx, y - 0.25 * dy],
-            [x + 0.5 * dx, y + 0.25 * dy]
-          ]]
-        })
-      }
-    }
-  }
+    const beta1 = polar2cartesian(options.tilt + 90)
 
-  return output.filter(grid => {
-    if (input.some(polygon => isInside(grid.address, polygon))) return true
+    const k0 = {
+      bound: [
+        dotProduct(beta0, forward([bbox[0], bbox[1]])),
+        dotProduct(beta0, forward([bbox[0], bbox[3]])),
+        dotProduct(beta0, forward([bbox[2], bbox[1]])),
+        dotProduct(beta0, forward([bbox[2], bbox[3]]))
+      ]
+    }
+    k0.minIndex = Math.floor(min(k0.bound))
+    k0.maxIndex = Math.ceil(max(k0.bound))
 
-    const points = []
+    const k1 = {
+      bound: [
+        dotProduct(beta1, forward([bbox[0], bbox[1]])),
+        dotProduct(beta1, forward([bbox[0], bbox[3]])),
+        dotProduct(beta1, forward([bbox[2], bbox[1]])),
+        dotProduct(beta1, forward([bbox[2], bbox[3]]))
+      ]
+    }
+    k1.minIndex = Math.floor(min(k1.bound))
+    k1.maxIndex = Math.ceil(max(k1.bound))
+
+    const getIntersection = linearSolver(beta0, beta1)
+
+    for (let i = k0.minIndex; i < k0.maxIndex; i++) {
+      output[i] = {}
+      for (let j = k1.minIndex; j < k1.maxIndex; j++) {
+        output[i][j] = {
+          properties: {
+            address: getIntersection(i + 0.5, j + 0.5)
+          },
+          coordinates: [[
+            getIntersection(i, j),
+            getIntersection(i, j + 1),
+            getIntersection(i + 1, j + 1),
+            getIntersection(i + 1, j)
+          ]]
+        }
+      }
+    }
+
     input.forEach(polygon => {
-      polygon.forEach(linearRing => {
-        points.push(...linearRing)
+      polygon.coordinates.forEach(linearRing => {
+        linearRing = linearRing.map(forward)
+        for (let n = 0; n < linearRing.length - 1; n++) {
+          let range = [
+            dotProduct(beta0, linearRing[n]),
+            dotProduct(beta0, linearRing[n + 1])
+          ]
+          let start = Math.floor(min(range) + 1)
+          let end = Math.ceil(max(range) - 1)
+          if (end >= start) {
+            const beta = [
+              linearRing[n + 1][1] - linearRing[n][1],
+              linearRing[n][0] - linearRing[n + 1][0]
+            ]
+            const k = linearRing[n][0] * linearRing[n + 1][1] -
+              linearRing[n][1] * linearRing[n + 1][0]
+            const getIntersection = linearSolver(beta0, beta)
+            for (let i = start; i <= end; i++) {
+              const intersection = getIntersection(i, k)
+              const j = Math.floor(dotProduct(beta1, intersection))
+              output[i][j].keep = true
+              output[i - 1][j].keep = true
+            }
+          }
+
+          range = [
+            dotProduct(beta1, linearRing[n]),
+            dotProduct(beta1, linearRing[n + 1])
+          ]
+          start = Math.floor(min(range) + 1)
+          end = Math.ceil(max(range) - 1)
+          if (end >= start) {
+            const beta = [
+              linearRing[n + 1][1] - linearRing[n][1],
+              linearRing[n][0] - linearRing[n + 1][0]
+            ]
+            const k = linearRing[n][0] * linearRing[n + 1][1] -
+              linearRing[n][1] * linearRing[n + 1][0]
+            const getIntersection = linearSolver(beta1, beta)
+            for (let j = start; j <= end; j++) {
+              const intersection = getIntersection(i, k)
+              const i = Math.floor(dotProduct(beta0, intersection))
+              output[i][j].keep = true
+              output[i][j - 1].keep = true
+            }
+          }
+        }
       })
     })
 
-    return points.some(point => isInside(point, grid.coordinates))
-  })
-}
-
-function isInside (point, polygon, ignoreHoles = false) {
-
+    // const minX = Math.floor((bbox[0] - options.center[0]) / dx)
+    // const maxX = Math.ceil((bbox[2] - options.center[0]) / dx)
+    // const minY = Math.floor((bbox[1] - options.center[1]) / dy)
+    // const maxY = Math.ceil((bbox[3] - options.center[1]) / dy)
+    // for (let nx = minX; nx <= maxX; nx++) {
+    //   for (let ny = minY; ny <= maxY; ny++) {
+    //     const x = options.center[0] + nx * dx
+    //     const y = options.center[1] + ny * dy
+    //     output.push({
+    //       type: 'Polygon',
+    //       id: [nx, ny].join('.').replace(/-/g, 'M'),
+    //       properties: {
+    //         address: [x, y]
+    //       },
+    //       coordinates: [[
+    //         [x + 0.5 * dx, y + 0.5 * dy],
+    //         [x - 0.5 * dx, y + 0.5 * dy],
+    //         [x - 0.5 * dx, y - 0.5 * dy],
+    //         [x + 0.5 * dx, y - 0.5 * dy]
+    //       ]]
+    //     })
+    //   }
+    // }
+  } else if (options.shape === 'hexagon') {
+    // const minX = Math.floor((bbox[0] - options.center[0]) / (dx * 0.75))
+    // const maxX = Math.ceil((bbox[2] - options.center[0]) / (dx * 0.75))
+    // for (let nx = minX; nx <= maxX; nx++) {
+    //   const cy = options.center[1] + nx * dy * 0.5
+    //   const minY = Math.floor((bbox[1] - cy) / dy)
+    //   const maxY = Math.ceil((bbox[3] - cy) / dy)
+    //   for (let ny = minY; ny <= maxY; ny++) {
+    //     const x = options.center[0] + nx * dx * 0.75
+    //     const y = cy + ny * dy
+    //     output.push({
+    //       type: 'Polygon',
+    //       id: [nx, ny].join('.').replace(/-/g, 'M'),
+    //       properties: {
+    //         address: [x, y]
+    //       },
+    //       coordinates: [[
+    //         [x + 0.5 * dx, y],
+    //         [x + 0.25 * dx, y + 0.5 * dy],
+    //         [x - 0.25 * dx, y + 0.5 * dy],
+    //         [x - 0.5 * dx, y],
+    //         [x - 0.25 * dx, y - 0.5 * dy],
+    //         [x + 0.25 * dx, y - 0.5 * dy]
+    //       ]]
+    //     })
+    //   }
+    // }
+  }
 }
